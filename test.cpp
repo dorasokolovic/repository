0cbcae738c62e7d2014b30901e70fdf4965cf087
dorasokolovic <sokolovicdora3@gmail.com>
2024-05-12 22:24:39 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
+#include<string>
+#include<fstream>
+#include<algorithm>
+
 using namespace std;
+
 int main(){
-    cout << "Hello world";
+    int a,b=0;
+    fstream datoteka;
+    string *imePrezime=new string[101];
+    datoteka.open("C:\\Users\\Sokol\\Documents\\imenik.txt");
+    string bla;
+    while(getline(datoteka, bla)){
+        imePrezime[b]=bla;
+        cout<<bla<<endl;
+        b++;
+    }
+    datoteka.close();
+    cin>>a;
+    cin.ignore();
+    for(int i=0;i<a;i++)
+        getline(cin,imePrezime[i+b]);
+    sort(imePrezime,imePrezime+a+b);
+    datoteka.open("C:\\Users\\Sokol\\Documents\\imenik.txt",ios::in|ios::out|ios::trunc);
+    for(int i=0;i<a+b;i++)
+        datoteka<<imePrezime[i]<<endl;
+    datoteka.close();
     return 0;
 }
